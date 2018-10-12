113fdf60fd0783f798b575c02bda82afebec3941
David Mortkowitz <dmortkowitz@gmail.com>
2018-10-12 10:35:34 -0700

    Add some initial backend logic for testing, including conversion of years to seconds, and sample code to test Mercury.

---
+++ src/galactic.js
@@ -0,0 +1,14 @@
+export class Galactic {
+  constructor(solarYears)
+  this.solarYears = solarYears;
+}
+
+YearsSeconds(solarYears) {
+  let ageSeconds = age * 31536000;
+  return ageSeconds;
+}
+
+mercuryYears (solarYears) {
+  let mercuryYears = solarYears / 0.24;
+  return Math.floor(mercuryYears);
+}
